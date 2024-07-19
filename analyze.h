a9c7cf5257c30f91354e856c4a8b32cf66b6d3af
rito1148 <rito1148@gmail.com>
2024-07-19 12:00:47 -0400

    test

---
+++ analyze.h
@@ -0,0 +1,10 @@
+int AnalyzeArp(u_char *data,int size);
+int AnalyzeIcmp(u_char *data,int size);
+int AnalyzeIcmp6(u_char *data,int size);
+int AnalyzeTcp(u_char *data,int size);
+int AnalyzeUdp(u_char *data,int size);
+int AnalyzeIp(u_char *data,int size);
+int AnalyzeIp6(u_char *data,int size);
+int AnalyzePacket(u_char *data,int size);
+
+
